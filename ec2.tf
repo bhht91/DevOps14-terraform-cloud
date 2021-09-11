b367ee7f2ae50b4cfd28c6e898add1b6cae966d2
Benji <87788154+bhht91@users.noreply.github.com>
2021-09-11 10:31:06 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
-resource “aws_instance” “sensitive” {
-    ami = “ami-087c17d1fe0178315”
-    instance_type = “t2.micro”
+resource "aws_instance" "sensitive" {
+    ami = "ami-087c17d1fe0178315"
+    instance_type = "t2.micro"
 
     tags = {
         Name = "terraform-cloud"
